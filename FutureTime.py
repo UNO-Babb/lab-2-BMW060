95730ecb286530d5c4ca9d7d85ffbb88a7bc0f35
BMW060 <brennan.m.wood@gmail.com>
2025-01-29 19:05:31 +0000

    FutureTime complete

--- FutureTime.py
+++ FutureTime.py
@@ -1,29 +1,34 @@
 #FutureTime.py
-#Name:
-#Date:
-#Assignment:
-
-# datetime will allow us to access the system date and time.
-import datetime
+#Name: Brennan Wood
+#Date: 1/29/25
+#Assignment: Lab 2 (FutureTime)
 
 def main():
-  #getting current time from system, storing to variable
-  now = datetime.datetime.now()
-  currentHour = now.hour
-  currentMinute = now.minute
-
-  print (currentHour, currentMinute) #this is just for checking, we should delete it later
-
-  #TODO:
-  #Ask user for hours
-  #Ask user for minutes
-
-  #Calculate the time after the user-supplied time has passed.
-
-  #Do not use any if statements in calculating the time.
-
-  #Output the future time in standard format "HH:MM"
-
-
+  print("\033[34mFutureTime\033[0m")
 if __name__ == '__main__':
-  main()
+    main()
+
+
+# importing date and time
+import datetime
+
+# storing to variable and showing the current time
+
+now = datetime.datetime.now()
+currentHour = str(now.hour)
+currentMinute = str(now.minute)
+
+print("\033[31mThe current time is "+currentHour.zfill(2)+":"+currentMinute.zfill(2), "\033[0m")
+
+# asking the user for hour and minute input
+
+print("How far in the future would you like to know the time?")
+plusHour = input("Number of hours: ")
+plusMinute = input("Number of minutes: ")
+
+# Math and printing the result
+
+futureHour = str((int(currentHour) + int(plusHour))%24+int(plusMinute)//60)
+futureMinute = str((int(currentMinute) + int(plusMinute))%60)
+print("\033[92mThe time in "+plusHour+" hours and "+plusMinute+" minutes will be "+futureHour.zfill(2)+":"+futureMinute.zfill(2)+"\033[0m")
+print("**All times shown are in 24 hour style time**")
